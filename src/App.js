db2a4585a91713e8b93208b8125e46c7a7d72159
Hilary <hilarybrittonfoster@gmail.com>
2023-02-15 12:22:07 -0800

    added favicon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Hompage/Homepage";
 import Library from "./pages/library/library";
 import NotFound from "./pages/NotFound/NotFound";
-// import Unread from "./pages/Unread/Unread";
 
 
 
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/library" element={<Library />}></Route>
-        {/* <Route path="/library/unread" element={<Unread />}></Route> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
